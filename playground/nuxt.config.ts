199a48f222e315bb7b7f3f1f027751a5b6427113
gyxoBka <gyxobka1996@gmail.com>
2025-04-24 01:14:43 +0300

    nuxt version update

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
+  compatibilityDate: '2025-04-24',
   svgSprite: {
     input: '~/assets/sprite/svg',
     output: '~/public/assets/sprite',
